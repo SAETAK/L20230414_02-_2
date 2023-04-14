27aff0783be1dd7b5dd36646052cf6d512533b8c
SAETAK <ihjang404@gmail.com>
2023-04-14 17:31:09 +0900

    Add WTF

--- Actor.cpp
+++ Actor.cpp
@@ -1,12 +1,16 @@
 #include "Actor.h"
 #include <iostream>
 #include <windows.h>
+#include "MyGameEngine.h"
 
 using namespace std;
 
 AActor::AActor()
     : X(1), Y(1), Shape(' ')
 {
+    R = 255;
+    G = 255;
+    B = 255;
 }
 
 AActor::AActor(int NewX, int NewY, char NewShape)
@@ -26,10 +30,14 @@ void AActor::Tick()
 
 void AActor::Render()
 {
-    COORD Pos;
-    Pos.X = X;
-    Pos.Y = Y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+    //COORD Pos;
+    //Pos.X = X;
+    //Pos.Y = Y;
+    //SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 
-	cout << Shape << endl;
+    //cout << Shape << endl;
+
+    SDL_SetRenderDrawColor(GEngine->MyRenderer, R, G, B, 255);
+    SDL_Rect MyRect = SDL_Rect{ (X -1) * 30, (Y-1) * 30, 30, 30 };
+    SDL_RenderFillRect(GEngine->MyRenderer, &MyRect);
 }
